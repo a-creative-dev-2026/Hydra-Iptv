4a28eb0c564a78e2240c01816632e7864bdcb1f9
A.Dev <vueiptv925@gmail.com>
2026-08-03 10:21:32 +0100

    Create channels.py

---
+++ channels.py
@@ -0,0 +1,229 @@
+# روابط مسبقة للقنوات حسب الدولة
+COUNTRY_CHANNELS = {
+    'tn': {
+        'name': 'تونس',
+        'channels': {
+            'Tunisia National TV': 'https://tn.tunisia.tv/stream.m3u8',
+            'Attessia TV': 'https://attessia.tv/live.m3u8',
+            'Hannibal TV': 'https://hannibaltv.tn/live.m3u8',
+            'Nessma TV': 'https://nessma.tv/stream.m3u8',
+            'El Hiwar Ettounsi': 'https://elhiwar.tn/live.m3u8',
+            'Zitouna TV': 'https://zitouna.tn/stream.m3u8',
+            'Carthage Plus': 'https://carthageplus.tn/live.m3u8',
+            'Tunisie 1': 'https://tunisie1.tn/stream.m3u8',
+            'Tunisie 2': 'https://tunisie2.tn/live.m3u8',
+        }
+    },
+    'sa': {
+        'name': 'السعودية',
+        'channels': {
+            'Saudi TV': 'https://sauditv.sa/live.m3u8',
+            'Al Ekhbariya': 'https://ekhbariya.sa/stream.m3u8',
+            'Saudi Sport': 'https://saudisport.sa/live.m3u8',
+            'MBC 1': 'https://mbc1.sa/stream.m3u8',
+            'MBC 2': 'https://mbc2.sa/live.m3u8',
+            'MBC Action': 'https://mbcaction.sa/stream.m3u8',
+            'MBC Drama': 'https://mbcdrama.sa/live.m3u8',
+        }
+    },
+    'eg': {
+        'name': 'مصر',
+        'channels': {
+            'ERTU 1': 'https://ertu1.eg/live.m3u8',
+            'ERTU 2': 'https://ertu2.eg/stream.m3u8',
+            'DMC': 'https://dmc.eg/live.m3u8',
+            'ON TV': 'https://ontv.eg/stream.m3u8',
+            'MBC Masr': 'https://mbcmasr.eg/live.m3u8',
+            'CBC': 'https://cbc.eg/stream.m3u8',
+        }
+    },
+    'dz': {
+        'name': 'الجزائر',
+        'channels': {
+            'EN TV': 'https://entv.dz/live.m3u8',
+            'Canal Algerie': 'https://canalalgerie.dz/stream.m3u8',
+            'A3 TV': 'https://a3tv.dz/live.m3u8',
+        }
+    },
+    'ma': {
+        'name': 'المغرب',
+        'channels': {
+            'Al Oula': 'https://aloula.ma/live.m3u8',
+            '2M': 'https://2m.ma/stream.m3u8',
+            'Arryadia': 'https://arryadia.ma/live.m3u8',
+        }
+    },
+    'ae': {
+        'name': 'الإمارات',
+        'channels': {
+            'Abu Dhabi TV': 'https://ad tv.ae/live.m3u8',
+            'Dubai TV': 'https://dubaitv.ae/stream.m3u8',
+            'Sharjah TV': 'https://sharjah.ae/live.m3u8',
+            'Al Arabiya': 'https://alarabiya.ae/stream.m3u8',
+        }
+    },
+    'jo': {
+        'name': 'الأردن',
+        'channels': {
+            'Jordan TV': 'https://jordantv.jo/live.m3u8',
+            'JRTV': 'https://jrtv.jo/stream.m3u8',
+        }
+    },
+    'lb': {
+        'name': 'لبنان',
+        'channels': {
+            'LBCI': 'https://lbci.lb/live.m3u8',
+            'MTV Lebanon': 'https://mtv.lb/stream.m3u8',
+            'NBN': 'https://nbn.lb/live.m3u8',
+        }
+    },
+    'kw': {
+        'name': 'الكويت',
+        'channels': {
+            'KTV 1': 'https://ktv1.kw/live.m3u8',
+            'KTV 2': 'https://ktv2.kw/stream.m3u8',
+            'Al Rai TV': 'https://alrai.kw/live.m3u8',
+        }
+    },
+    'qa': {
+        'name': 'قطر',
+        'channels': {
+            'Qatar TV': 'https://qatar tv.qa/live.m3u8',
+            'Al Rayyan': 'https://alrayyan.qa/stream.m3u8',
+            'beIN Sports 1': 'https://bein1.qa/live.m3u8',
+            'beIN Sports 2': 'https://bein2.qa/stream.m3u8',
+        }
+    },
+    'om': {
+        'name': 'عمان',
+        'channels': {
+            'Oman TV': 'https://omantv.om/live.m3u8',
+            'Oman Sport': 'https://omansport.om/stream.m3u8',
+        }
+    },
+    'bh': {
+        'name': 'البحرين',
+        'channels': {
+            'Bahrain TV': 'https://bahraintv.bh/live.m3u8',
+            'Bahrain Sport': 'https://bahrainsport.bh/stream.m3u8',
+        }
+    },
+    'iq': {
+        'name': 'العراق',
+        'channels': {
+            'Iraq TV': 'https://iraqtv.iq/live.m3u8',
+            'Al Iraqiya': 'https://aliraqiya.iq/stream.m3u8',
+        }
+    },
+    'sy': {
+        'name': 'سوريا',
+        'channels': {
+            'Syria TV': 'https://syriatv.sy/live.m3u8',
+            'Syria Drama': 'https://syriadrama.sy/stream.m3u8',
+        }
+    },
+    'ps': {
+        'name': 'فلسطين',
+        'channels': {
+            'Palestine TV': 'https://palestinetv.ps/live.m3u8',
+            'PBC': 'https://pbc.ps/stream.m3u8',
+        }
+    },
+    'ly': {
+        'name': 'ليبيا',
+        'channels': {
+            'Libya TV': 'https://libyatv.ly/live.m3u8',
+            'Libya Sport': 'https://libyasport.ly/stream.m3u8',
+        }
+    },
+    'sd': {
+        'name': 'السودان',
+        'channels': {
+            'Sudan TV': 'https://sudantv.sd/live.m3u8',
+            'Sudan Sport': 'https://sudansport.sd/stream.m3u8',
+        }
+    },
+    'ye': {
+        'name': 'اليمن',
+        'channels': {
+            'Yemen TV': 'https://yementv.ye/live.m3u8',
+            'Yemen Sport': 'https://yemensport.ye/stream.m3u8',
+        }
+    },
+    'us': {
+        'name': 'الولايات المتحدة',
+        'channels': {
+            'CNN': 'https://cnn.com/live.m3u8',
+            'FOX': 'https://fox.com/stream.m3u8',
+            'NBC': 'https://nbc.com/live.m3u8',
+            'ABC': 'https://abc.com/stream.m3u8',
+            'CBS': 'https://cbs.com/live.m3u8',
+        }
+    },
+    'uk': {
+        'name': 'بريطانيا',
+        'channels': {
+            'BBC': 'https://bbc.co.uk/live.m3u8',
+            'ITV': 'https://itv.com/stream.m3u8',
+            'Channel 4': 'https://channel4.com/live.m3u8',
+            'Sky News': 'https://skynews.com/stream.m3u8',
+        }
+    },
+    'fr': {
+        'name': 'فرنسا',
+        'channels': {
+            'TF1': 'https://tf1.fr/live.m3u8',
+            'France 2': 'https://france2.fr/stream.m3u8',
+            'France 3': 'https://france3.fr/live.m3u8',
+            'Canal+': 'https://canalplus.fr/stream.m3u8',
+        }
+    },
+    'de': {
+        'name': 'ألمانيا',
+        'channels': {
+            'ARD': 'https://ard.de/live.m3u8',
+            'ZDF': 'https://zdf.de/stream.m3u8',
+            'RTL': 'https://rtl.de/live.m3u8',
+            'ProSieben': 'https://prosieben.de/stream.m3u8',
+        }
+    },
+    'it': {
+        'name': 'إيطاليا',
+        'channels': {
+            'RAI 1': 'https://rai1.it/live.m3u8',
+            'RAI 2': 'https://rai2.it/stream.m3u8',
+            'Mediaset': 'https://mediaset.it/live.m3u8',
+        }
+    },
+    'es': {
+        'name': 'إسبانيا',
+        'channels': {
+            'TVE': 'https://tve.es/live.m3u8',
+            'Antena 3': 'https://antena3.es/stream.m3u8',
+            'La Sexta': 'https://lasexta.es/live.m3u8',
+        }
+    },
+    'tr': {
+        'name': 'تركيا',
+        'channels': {
+            'TRT 1': 'https://trt1.tr/live.m3u8',
+            'TRT Haber': 'https://trthaber.tr/stream.m3u8',
+            'Kanal D': 'https://kanald.tr/live.m3u8',
+            'Show TV': 'https://showtv.tr/stream.m3u8',
+        }
+    }
+}
+
+# روابط خاصة للقنوات الرياضية العالمية
+SPORTS_CHANNELS = {
+    'beIN Sports 1': 'https://bein1.qa/live.m3u8',
+    'beIN Sports 2': 'https://bein2.qa/stream.m3u8',
+    'beIN Sports 3': 'https://bein3.qa/live.m3u8',
+    'Sky Sports': 'https://skysports.com/live.m3u8',
+    'ESPN': 'https://espn.com/stream.m3u8',
+    'FOX Sports': 'https://foxsports.com/live.m3u8',
+    'NBC Sports': 'https://nbcsports.com/stream.m3u8',
+    'DAZN': 'https://dazn.com/live.m3u8',
+    'Eurosport': 'https://eurosport.com/stream.m3u8',
+    'Sport TV': 'https://sporttv.com/live.m3u8',
+}
